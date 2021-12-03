07f19598d220eca6134feb1a6bc31a5ae9fba4cb
Billy <billy.luong7@gmail.com>
2021-12-03 15:25:43 -0500

    got api call to work

--- frontend/src/pages/search-page.jsx
+++ frontend/src/pages/search-page.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import  React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SearchPage() {
-    const handleSearch = () => {
+    const navigate = useNavigate();
+    let loading = false;
+    let [data,setData] = useState('');
 
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        await axios.get('/search')
+            .then(res => {
+                setData(res.data);
+                loading = true;
+            })
+            .catch(err => {
+                console.log(err);         
+            }
+        );
+       
     }
+
+    useEffect(() => {
+        console.log('in here')
+        if (data) {
+            console.log('in effect')
+            loading = false;
+            navigate('/result', { state: { data }  });
+        }
+    });
 
     return (
         <div>
-            <h1>Search</h1>
-            {/* search box */}
-            {/* button to make api call here... don't worry about this, just make the button */}
+            {
+                !loading ? 
+                    <div>
+                        <h1>Search</h1>
+                        <form onSubmit={(e) => handleSearch(e)}>
+                            Query: <input type="text" name="query"/>
+                            <input type="submit" value="Submit"/>
+                        </form>
+                    </div>
+                :
+                    <div>
+                        <p>loading...</p>
+                    </div>
+            }
         </div>
     );
 }
