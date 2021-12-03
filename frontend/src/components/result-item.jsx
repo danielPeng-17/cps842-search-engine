c9e5792c28edb4301df4ad90d67e4fa0ed69dd18
Billy <billy.luong7@gmail.com>
2021-12-03 18:59:10 -0500

    formatted the pages to look a bit better

--- frontend/src/components/result-item.jsx
+++ frontend/src/components/result-item.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function ResultItem(props) {
-
+    console.log(props)
+    // put the url link to a variable and remove https:// from website url
+    var url = (props.data.url).replace(/^https?:\/\/www./, '')
     return (
-        <div>
-            {console.log(props)}
-            <h1>title: {props.data.title}</h1>
-            <p>url is: {props.data.url}</p>
-            <p>score</p>
-        </div>
+        <Card style={{marginRight:"auto", marginLeft:"auto", width: '90%', fontSize: '30px', fontFamily: 'arial', paddingBottom: "10px", paddingTop: "10px"}}>
+        <Card.Body>
+            {/* url[0].toUpperCase() + url.substring(1) uppercases the first word */}
+            <Card.Title style={{paddingBottom:"1px"}}><a style={{fontSize: "30px"}} href={props.data.url}> {url[0].toUpperCase() + url.substring(1)} </a></Card.Title>
+            {/* Uncomment the one below if you just want the link */}
+            {/* <Card.Title style={{paddingBottom:"1px"}}><a style={{fontSize: "30px"}} href={props.data.url}> {props.data.url} </a></Card.Title>  */} 
+            <Card.Text style={{marginTop:"5px", marginBottom: "5px", fontWeight: "bold"}}>
+            The ranking score is {props.data.score}
+            </Card.Text>
+        </Card.Body>
+        </Card>
     );
 }
 
