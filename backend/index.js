0ff6d2760aee312a88a6e70baff429b594f57265
Daniel Peng <danielpeng03172000@gmail.com>
2021-12-03 00:18:08 -0500

    test data

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get('/crawl_pages', async (req, res) => {
     res.send('Web pages have been crawled.');
 });
 
-app.get('/', (req, res) => {
+app.get('/search', (req, res) => {
     // const query = req.query.search;
     // console.log(query);
     // let tempQuery = 'double-click on the WorldWideWeb icon';
@@ -45,7 +45,12 @@ app.get('/', (req, res) => {
     // NOTE: w1 is WEIGHT_1 and w2 is WEIGHT_2 from constants.js
 
     // return object of results -> ordered by ranking score
-    res.send('hello world');
+    const testData = {
+        1: {'title': 'Google title', 'url': 'https://www.google.ca', "score": 0.885634},
+        2: {'title': 'Amazon title','url': 'https://www.amazon.com', "score": 0.55734},
+        3: {'title': 'Netflix title','url': 'https//www.netflix.com', "score": 0.14564}
+    }
+    res.json(testData);
 });
 
 app.listen(port, () => {
