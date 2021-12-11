857e956fd765f8ba40b9f0dad7d5389d94caa71b
Mabel Le <mabelle.ky@gmail.com>
2021-12-10 20:12:37 -0500

    - implemented matrix 2

--- backend/utils/page-rank.js
+++ backend/utils/page-rank.js
@@ -5,7 +5,7 @@ var alpha = 0.16 //other common alpha values also 0.01 and 0.10, 0.05
 var totalNumOfLinks = 4 // N value
 var countOfOnesPerRow = [] // Counting # of 1s per row on Adjacency Matrix
 
-// 1) Build Adjacency matrix based on link in seed file and outlinks assocated with seed file link
+// 1) Build Adjacency matrix based on link in links.txt and outlinks associated with links.txt
 /*var adjacencyMatrix =  [[0,1,1,1],
                         [0,0,0,0],
                         [0,1,0,1],
@@ -24,36 +24,32 @@ const createAdjacencyMatrix = () => {
     }
     
     for (let [key, item] of data) {
-        // console.log(item.links[0]);
         var count = 0;
         var adjacencyRow = [];
         for (let url of matrixUrls) {
-            // if (count > 1) break;
-            // console.log(crawler.getUrl("", "", item.links[0], url));
+            //TODO handle ../ aka relative paths
             let result = item.links.find(element => {
-                // console.log(crawler.getUrl("", "", element, url));
-                // element == crawler.getUrl("", "", element, url);
-                return url.includes(element)
+                if (!element.startsWith("./") && element != "/") {
+                    return url.includes(element)
+                }
+                return false;
             });
             if (result) {
                 adjacencyRow.push(1);
             } else {
                 adjacencyRow.push(0);
             }
-
-            // count++;
         }
         adjacencyMatrix.push(adjacencyRow);
-        // console.log(adjacencyRow);
-        // break;
     }
-    console.log(adjacencyMatrix[150]);
+    let index = 0;
+    console.log("Adjacency Matrix for " + index);
+    console.log(adjacencyMatrix[index]);
+    return adjacencyMatrix;
 }
 
-createAdjacencyMatrix();
-
 // 2) Conditions for buidling 2nd matrix:
-// - If the entire row has '0' values only or countOfOnesPerRow is '0', each cell in that row
+// x - If the entire row has '0' values only or countOfOnesPerRow is '0', each cell in that row
 // has the value of 1/totalNumOfLinks
 //
 // - If the entire row is not filled with '0' values only in Adjacency matrix, each cell with value '0' 
@@ -61,21 +57,41 @@ createAdjacencyMatrix();
 //
 // - each cell with value '1' in Adjacency matrix = 1/countOfOnesPerRow for the row that the cell is on
 
-const matrixTwo = (adjacencyMatrix, totalNumOfLinks, countOfOnesPerRow) => {
-    var matrixTwo = [[]];
+const createMatrixTwo = () => {
+    let adjacencyMatrix = createAdjacencyMatrix();
+    let totalLinks = adjacencyMatrix.length;
 
+    //pseudo code for map
+    // var newMatrix [[]];
+    // foreach x in adjacencyMatrix {
+        // do some stuff and add to newMatrix
+    // }
 
+    let newMatrix = adjacencyMatrix.map((value) => {
+        //all zero check
+        if (value.indexOf(1) == -1) {
+            let evenSplit = 1 / totalLinks
+            return value.map(x => x = evenSplit);
+        }
+        var numberOfOnes = 0;
+        value.forEach((value) => {
+            if (value == 1) numberOfOnes++;
+        });
+        let multiplier = 1 / numberOfOnes;
+        return value.map(x => x * multiplier);
+    });
+    return newMatrix;
 }
-
-
 
 // 3) Conditions for building 3rd matrix:
 // - denoting each cell from 2nd matrix as x
 // - If the entire row has only '0' values in Adjacency matrix, cell value remains the same as 2nd matrix
 // - each cell in 3rd matrix is x * (1 - alpha)
 
-
-
+const createMatrixThree = () => {
+    let matrixTwo = createMatrixTwo();
+}
+matrixThree();
 
 // 4) Conditions for building 4th matrix:
 // - denoting each cell from 3rd matrix as P
